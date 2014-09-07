9655160d89f423ebb14f3dfb2b8357360d4bfc0e
Jasper den Ouden <o.jasper@gmail.com>
2014-09-07 18:05:21 +0200

    test_trie_easy.go more commandliney

--- src/test_trie_easy.go
+++ src/test_trie_easy.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"flag"
 	"math/rand"
 	"trie_easy"
 
@@ -13,25 +14,33 @@ import (
 
 func main() {
 
-	seed := time.Now().UnixNano()
-	r := rand.New(rand.NewSource(seed))
+	var seed, n_min, n_max, N int64
+	flag.Int64Var(&seed, "seed", time.Now().UnixNano(), "Random seed for test.")
+	flag.Int64Var(&n_min, "n_min", 0, "Minimum length of random chunk.")
+	flag.Int64Var(&n_max, "n_max", 64, "Maximum length of random chunk.")
+	flag.Int64Var(&N, "N", 80, "Number of entries.")
+	var print bool
+	flag.BoolVar(&print, "print", false, "Whether to print everything")
+
 	node := trie_easy.NewTrie(nil)
-	compare := map[string]int{}
-	
-	n_min, n_max, N := int64(3), int64(8), 10
+	compare := map[string]int64{}
+
+	r := rand.New(rand.NewSource(seed))
 	
 	fmt.Println("= Put stuff in twice.")
-	for i := 0 ; i < N ; i++ {
+	for i := int64(0) ; i < N ; i++ {
 		chunk := test_common.Rand_chunk(r, n_min, n_max)
-		fmt.Println(i, hex.EncodeToString(chunk))
+		if print { fmt.Println(i, hex.EncodeToString(chunk)) }
 		node.Set(chunk, i, trie_easy.Creator16{})
 		compare[string(chunk)] = i
 	}
-	fmt.Println("= Printing")
-	node.MapAll(nil, func(data interface{}, k []byte, v interface{}) bool{
-		fmt.Println(v, hex.EncodeToString(k))
-		return false
-	})
+	if print {
+		fmt.Println("= Printing")
+		node.MapAll(nil, func(data interface{}, k []byte, v interface{}) bool{
+			fmt.Println(v, hex.EncodeToString(k))
+			return false
+		})
+	}
 	fmt.Println("= And check equality.")
 	for k,v := range compare {
 		val := node.Get([]byte(k), 0)
@@ -40,7 +49,7 @@ func main() {
 		}
 	}
 	fmt.Println("= Try against false positives.")
-	for i := 0 ; i < N ; i++ {
+	for i := int64(0) ; i < N ; i++ {
 		key := test_common.Rand_chunk(r, n_min, n_max)
 		v, got := compare[string(key)]
 		val := node.Get([]byte(key), 0)
