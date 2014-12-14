cb104d4261c57c026e27a286fbfa43333922f9ff
Jasper den Ouden <o.jasper@gmail.com>
2014-12-15 00:59:57 +0100

    Bit of a false start.. now it works

--- contracts/merkle.py
+++ contracts/merkle.py
@@ -6,8 +6,12 @@ t = pyethereum.tester  # For t.sha3
 def eth_num(x):  # TODO wrong way around? (should be fixing t.sha3?)
     return x + 2**256 if x < 0 else x
 
+def deth_num(x):
+    assert x < 2**256
+    return x - 2**256 if x > 2**255 else x
+
 def eth_lt(x,y):
-    return x < y if x < 2**255 else x > y
+    return deth_num(x) <= deth_num(y)
 
 def h2(x, y):
     return t.sha3([x,y] if eth_lt(x, y) else [y,x])
@@ -21,21 +25,50 @@ def path_w_root(sides):
 
 # Element for moving root, with option of indicating interest(or lack)
 class Element:
-    def __init__(self, H, depth, interest):
+    def __init__(self, H, depth, up, left, right, data=None):
         self.H = H
         self.depth = depth
-        self.interest = interest
+        self.up = up
+        self.right = right
+        self.left = left
+        self.data = data
+        if self.right is not None:
+            assert self.left is not None
+            assert eth_lt(self.left.H, self.right.H)
+
+    def selftest(self):
+        if self.right is not None:
+            assert self.left is not None
+            
+            assert self.H == t.sha3([self.left.H, self.right.H])
+            assert eth_lt(self.left.H, self.right.H)
+        else:
+            assert self.left is None
+            
+        if self.up is not True:
+            assert (self is self.up.left) or (self is self.up.right)
+            self.up.selftest()
         
     def el_path(self):
-        assert self.interest  # Will only be a path to track if there is interest.
-        if self.interest == True:  # End of the line.
-            return [self]
+        return [self] + self.up._el_path(self)
+
+    def _el_path(self, avoid):
+        assert self.up  # Will only be a path to track if there is interest.
+        assert (self.left is avoid) or (self.right is avoid)
+        ret = [self.left if (self.right is avoid) else self.right]
+        if self.up is True: # Top.
+            return ret
         else:
-            return [self] + self.interest.el_path()
+            return ret + self.up._el_path(self)
 
     def path(self):
         return map(lambda(el):el.H, self.el_path())
 
+
+def Element_from_two(x, y):
+    l,r = ((x, y) if eth_lt(x.H, y.H) else (y, x))
+    assert eth_lt(l.H, r.H), (eth_lt(x.H, y.H), eth_lt(y.H, x.H), x.H,y.H)
+    return Element(t.sha3([l.H, r.H]), max(l.depth, r.depth) + 1, l.up or r.up, l, r)
 
 # Moving root.
 class MovingRoot:
@@ -46,30 +79,29 @@ class MovingRoot:
         return MovingRoot(self.list)
 
     def incorporate(self, chunk, interest=False):
-        return self.incorporate_H(t.sha3(chunk), interest)
-
-    def incorporate_H(self, H, interest=False):
-        el = Element(H, 1, interest)
+        el = Element(t.sha3(chunk), 0, interest, None, None, chunk)
         self.list.append(el)
         self.simplify()
         return el
 
     # Forcing makes "lobsized" trees with uneven depth.(for if element count != 2**n)
-    def simplify(self, force = False):
+    def simplify(self, force=False):
         while len(self.list) >= 2:
             last = self.list[-1]
             prelast =  self.list[-2]
             if last.depth != prelast.depth and not force:  # Not equal depth, done..
                 return
+            
             # Combine equal depth tree tops.
             self.list = self.list[:-2]
-            el = Element(h2(last.H, prelast.H), last.depth + 1,\
-                         last.interest or prelast.interest)
+            el = Element_from_two(prelast, last)
             self.list.append(el)
-            if last.interest:  # Refer on if interested.
-                last.interest = el
-            if prelast.interest:
-                prelast.interest = el
+            if last.up:  # Refer on if interested.
+                assert last.up is True
+                last.up = el
+            if prelast.up:
+                assert prelast.up is True
+                prelast.up = el
 
     # Force-combine lists, returning the current result.
     def finalize(self, dont_change=False):
@@ -79,4 +111,4 @@ class MovingRoot:
         assert len(self.list) > 0
         self.simplify(True)
         assert len(self.list) == 1
-        return self.list[0].H
+        return self.list[0]
--- contracts/test/test_merkle.py
+++ contracts/test/test_merkle.py
@@ -1,7 +1,7 @@
 import pyethereum
 t = pyethereum.tester
 
-from merkle import eth_num, path_w_root, MovingRoot, h2
+from merkle import deth_num, eth_num, path_w_root, MovingRoot, h2
 
 from random import randrange, random
 
@@ -17,15 +17,20 @@ def randval():
 
 # Doesnt test merkle trees, but instead just faked paths.
 def test_random_path(n):
-    path = map(lambda(x): randval(), range(n))
+    test_path(map(lambda(x): randval(), range(n)))
+
+def test_path(path, expect_root=None):
     root = path_w_root(path)
+    if expect_root is not None:
+        assert root == expect_root
     r = s.send(t.k0,  c, 0, path)
     r2 = s.send(t.k0, c2, 0, path)
     assert len(r) == 1 and len(r2) == 1
-
     assert eth_num(r[0]) == root and eth_num(r2[0]) == root
 
-def test_random_interest(n, interest_p=0.5, m=6):
+def test_random_interest(n, interest_p=0.5, simulate_p=None, m=6):
+    if simulate_p is None:
+        simulate_p = 10.0/n
     mr = MovingRoot()
     interesting = []
     for _ in range(n):
@@ -34,16 +39,29 @@ def test_random_interest(n, interest_p=0.5, m=6):
         if interest:
             interesting.append(got)
     root = mr.finalize()
+    k = 0
     for el in interesting:
-        assert path_w_root(el.path()[-1:]) == root
+        k += 1
+        el.selftest()
+        path = el.path()
+        assert path_w_root(path) == root.H
+        s.mine()
+        if random() < simulate_p:
+            print("simulating %d" % k)
+            test_path(path, root.H)
 
 for _ in range(10):
-    test_random_interest(60)
-
-for _ in range(10):
+    s.mine()    
     x, y = randval(), randval()
     assert h2(x,y)  == h2(y,x)
+    assert deth_num(eth_num(x)) == x
+    assert deth_num(eth_num(-x)) == -x
 
 for _ in range(10):
+    s.mine()    
     test_random_path(10)
     
+
+for k in range(1):
+    print(k)    
+    test_random_interest(600)
