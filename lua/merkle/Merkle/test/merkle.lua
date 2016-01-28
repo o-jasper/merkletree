b0d22528f47ce393d7e89a7dc4a9f62a34090f35
Jasper den Ouden <o.jasper@gmail.com>
2016-01-28 01:11:06 +0100

    Test with random generation, doesnt work for non-power-of-two, apparently

--- lua/merkle/Merkle/test/merkle.lua
+++ lua/merkle/Merkle/test/merkle.lua
@@ -17,6 +17,47 @@ local function test_example()
    local proof = provable:produce_proof()
 
    assert( verify:verify(root_H, proof, "two") )
+   assert( not verify:verify(root_H, proof, "wrong") )
 end
 
 test_example()
+
+local function rand_str(lmin, lmax)
+   local i, n, ret = 0, math.random(lmin or 4, lmax or 10), {}
+   while i < n do
+      table.insert(ret, string.char(math.random(33, 126)))
+      i = i + 1
+   end
+   return table.concat(ret)
+end
+
+local function test_blast(n, prove_p)
+   local tree = Tree:new{ H = H }
+
+   local provables = {}
+   while n > 0 do
+      local data = rand_str()
+      if math.random() < prove_p then
+         local node = tree:add(data, true)
+         node.data = data
+         assert(node.H == H(data))
+         table.insert(provables, node)
+      else
+         tree:add(data)
+      end
+      n = n - 1
+   end
+   local root_H = tree:finish()
+
+   for i, node in ipairs(provables) do
+      local proof = node:produce_proof()
+      assert(verify:verify(root_H, proof, node.data), string.format("Failed %d", i))
+   end
+end
+
+local m = 10
+while m > 0 do
+   print(m)
+   test_blast(128, 0.1)
+   m = m - 1
+end
