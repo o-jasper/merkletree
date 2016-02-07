fa3b77763edc2ea976022f228d58553b72138926
Jasper den Ouden <o.jasper@gmail.com>
2016-02-07 15:10:29 +0100

    Test consistency with class a bit

--- lua/merkle/test/bin.lua
+++ lua/merkle/test/bin.lua
@@ -2,6 +2,15 @@ local sha2 = require 'merkle.sha2'
 local enhex = require "merkle.enhex"
 
 local fd = io.open("/dev/stdin")
-local str = fd:read("l")
+local str = fd:read("l") or ""
 fd:close()
-print(enhex(sha2.hash256(str or "")))
+
+-- Check consistency with class.
+if math.random(4) == 1 then
+   assert( sha2.hash256(str) == sha2.Hash256:new{str}:close() )
+else
+   local i = math.random(#str)
+   assert( sha2.hash256(str) ==
+              sha2.Hash256:new{string.sub(str, 1, i), string.sub(str, i+1)}:close() )
+end
+print(enhex(sha2.hash256(str)))
